cbed828667cf56f39b2c1d59ea864fbe901de148
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2019-01-21 14:45:58 +0300

    Practical jQuery

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,19 +1,28 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
 
-console.log("Hello Sivar");
+    console.log("Hello Sivar");
 
-var theForm = $("#theForm");
-theForm.hide();
-//theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
+    //theForm.hidden = true;
 
-var button = $("#buyButton");
-button.on("click", function () {
-    console.log("Buying Item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+    //var listItems = productInfo.item[0].children;
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
 });
-
-var productInfo = $(".product-props li");
-productInfo.on("click", function () {
-    console.log("You clicked on " + $(this).text());
-});
-//var listItems = productInfo.item[0].children;
