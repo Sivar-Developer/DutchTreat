322891a757ddc38979c4674288e83e2f0fa7a503
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2019-01-21 10:40:34 +0300

    Javascript events

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -5,3 +5,11 @@ console.log("Hello Sivar");
 
 var theForm = document.getElementById("theForm");
 theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementByClassName("product-props");
+var listItems = productInfo.item[0].children;
