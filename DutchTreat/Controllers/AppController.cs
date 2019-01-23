08e4305f7b04b8718af80984798b86f8b8f6d621
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2019-01-23 12:36:37 +0300

    Adding a Service

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,13 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             //throw new InvalidOperationException("Bad things happen");
@@ -27,6 +35,9 @@ namespace DutchTreat.Controllers
             if (ModelState.IsValid)
             {
                 // Send the email
+                _mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
             }
             else
             {
