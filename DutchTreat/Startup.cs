b011a1f006e650633738f185e5b93581eecd60ca
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2019-01-28 13:59:01 +0300

    Using Entity Framework core tooling

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -16,6 +17,7 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DutchContext>();
             services.AddTransient<IMailService, NullMailService>();
             services.AddMvc();
         }
