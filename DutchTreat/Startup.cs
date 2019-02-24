e8757e587803046190aebffffdca4dc373e97fc5
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2019-02-24 14:49:40 +0300

    Configurating Identity

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using DutchTreat.Data;
+using DutchTreat.Data.Entities;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +29,12 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<StoreUser, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<DutchContext>();
+
             services.AddDbContext<DutchContext>(cfg => {
                 cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
             });
@@ -58,6 +66,8 @@ namespace DutchTreat
             //app.UseDefaultFiles();
 
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(cfg =>
             {
